9d9a19c51349d79a65a6cc752d7f0421fbc29f7d
Martin Billard <martin.billard@toulouse.miage.fr>
2024-06-04 10:50:46 +0200

    Ajout du feign et eureka permettant la mesure selon un timing

--- src/main/java/ag/agriconnectreleves/AgriconnectRelevesApplication.java
+++ src/main/java/ag/agriconnectreleves/AgriconnectRelevesApplication.java
@@ -5,9 +5,13 @@ import ag.agriconnectreleves.repo.ReleveRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
-//@EnableDiscoveryClient
+
 @SpringBootApplication
+@EnableDiscoveryClient
+@EnableFeignClients
 public class AgriconnectRelevesApplication implements CommandLineRunner {
 
     private final ReleveRepository releveRepository;
